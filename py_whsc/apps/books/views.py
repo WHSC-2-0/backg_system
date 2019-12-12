c3775f4096991695cbe7c748b65364ac6b0b08fb
dabaicai233 <2272096201@qq.com>
2019-12-12 09:08:31 +0800

    add book base

--- py_whsc/apps/books/views.py
+++ py_whsc/apps/books/views.py
@@ -11,23 +11,26 @@ class WhBookViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
                      mixins.RetrieveModelMixin, mixins.CreateModelMixin):
     queryset = WhBook.objects.all()
     serializer_class = WhBookSerializer
-    # authentication_classes = (WhAdminTokenAuthentication,)
+    authentication_classes = (WhAdminTokenAuthentication,)
 
     # 新增书籍
     @list_route(methods=["POST"], serializer_class=WhBookSerializer)
-    def add_book(self):
+    def add_book(self, request, *args, **kwargs):
+        c_id = request.query_params.get("c_id")
+        status = request.query_params.get("status")
+        
 
         """
         此处获取用户选择的数据
 
         """
-        new_book = WhBook('字段名=获取值...')
+        new_book = WhBook(b_name='b_name')
         new_book.save()
         return Response('Save OK!')
 
     # 章节列表
     @list_route(methods=['POST'], serializer_class=WhCatalogSerializer)
-    def chapter_view(self):
+    def chapter_view(self, *args, **kwargs):
         res = WhCatalog.objects.all()
         if res:
             return Response(res)
@@ -36,7 +39,7 @@ class WhBookViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
 
     # 新增章节
     @list_route(methods=['POST'])
-    def add_catalog(self):
+    def add_catalog(self, *args, **kwargs):
         """
         此处获取用户上传的数据
         """
@@ -45,7 +48,7 @@ class WhBookViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
         return Response('Insert OK!')
 
     # 编辑（章节）
-    def update_book(self):
+    def update_book(self, *args, **kwargs):
         """
         此处获取用户上传的数据
         """
@@ -53,7 +56,7 @@ class WhBookViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
         return Response('Update OK!')
 
     # 删除
-    def delete_book(self):
+    def delete_book(self, *args, **kwargs):
         """
         此处获取用户上传的数据
         """
